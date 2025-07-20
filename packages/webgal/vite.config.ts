a8fc5cd967a5a2b23ac6bbcabf8c4ae2df3c0005
boomwwww <boom_w_w@outlook.com>
2025-07-20 17:15:38 +0800

    pixi导入插件

--- packages/webgal/vite.config.ts
+++ packages/webgal/vite.config.ts
@@ -3,57 +3,66 @@ import react from '@vitejs/plugin-react';
 import loadVersion from 'vite-plugin-package-version';
 import { resolve, relative } from 'path';
 import { visualizer } from 'rollup-plugin-visualizer';
-import { readdirSync, watch, writeFileSync } from 'fs';
-import { isEqual } from 'lodash';
+// import { readdirSync, watch, writeFileSync } from 'fs';
+// import { isEqual } from 'lodash';
 import Info from 'unplugin-info/vite';
+
+import pixiPerformAutoImport from './src/plugins/pixi-perform-auto-import';
 
 import webgalGo from './src/webgal-go/vite-plugin/index';
 
 // https://vitejs.dev/config/
 
 // @ts-ignore
-const env = process.env.NODE_ENV;
-console.log(env);
-(() => {
-  const pixiPerformScriptDirPath = './src/Core/gameScripts/pixi/performs/';
-  const pixiPerformManagerDirPath = './src/Core/util/pixiPerformManager/';
-  const relativePath = relative(pixiPerformManagerDirPath, pixiPerformScriptDirPath).replaceAll('\\', '/');
-  let lastFiles: string[] = [];
+// const env = process.env.NODE_ENV;
+// console.log(env);
+// (() => {
+//   const pixiPerformScriptDirPath = './src/Core/gameScripts/pixi/performs/';
+//   const pixiPerformManagerDirPath = './src/Core/util/pixiPerformManager/';
+//   const relativePath = relative(pixiPerformManagerDirPath, pixiPerformScriptDirPath).replaceAll('\\', '/');
+//   let lastFiles: string[] = [];
 
-  function setInitFile() {
-    console.log('正在自动编写pixi特效依赖注入');
-    writeFileSync(
-      resolve(pixiPerformManagerDirPath, 'initRegister.ts'),
-      lastFiles
-        .map((v) => {
-          const filePath = relativePath + '/' + v.slice(0, v.lastIndexOf('.'));
-          return `import '${filePath}';`;
-        })
-        .join('\n') + '\n',
-      { encoding: 'utf-8' },
-    );
-  }
+//   function setInitFile() {
+//     console.log('正在自动编写pixi特效依赖注入');
+//     writeFileSync(
+//       resolve(pixiPerformManagerDirPath, 'initRegister.ts'),
+//       lastFiles
+//         .map((v) => {
+//           const filePath = relativePath + '/' + v.slice(0, v.lastIndexOf('.'));
+//           return `import '${filePath}';`;
+//         })
+//         .join('\n') + '\n',
+//       { encoding: 'utf-8' },
+//     );
+//   }
 
-  function getPixiPerformScriptFiles() {
-    const pixiPerformScriptFiles = readdirSync(pixiPerformScriptDirPath, { encoding: 'utf-8' }).filter((v) =>
-      ['ts', 'js', 'tsx', 'jsx'].includes(v.slice(v.indexOf('.') + 1, v.length)),
-    );
-    if (!isEqual(pixiPerformScriptFiles, lastFiles)) {
-      lastFiles = pixiPerformScriptFiles;
-      setInitFile();
-    }
-  }
+//   function getPixiPerformScriptFiles() {
+//     const pixiPerformScriptFiles = readdirSync(pixiPerformScriptDirPath, { encoding: 'utf-8' }).filter((v) =>
+//       ['ts', 'js', 'tsx', 'jsx'].includes(v.slice(v.indexOf('.') + 1, v.length)),
+//     );
+//     if (!isEqual(pixiPerformScriptFiles, lastFiles)) {
+//       lastFiles = pixiPerformScriptFiles;
+//       setInitFile();
+//     }
+//   }
 
-  getPixiPerformScriptFiles();
+//   getPixiPerformScriptFiles();
 
-  if (env !== 'production') watch(pixiPerformScriptDirPath, { encoding: 'utf-8' }, getPixiPerformScriptFiles);
-})();
+//   if (env !== 'production') watch(pixiPerformScriptDirPath, { encoding: 'utf-8' }, getPixiPerformScriptFiles);
+// })();
 
 export default defineConfig({
   plugins: [
     react(),
     loadVersion(),
     Info(),
+    pixiPerformAutoImport({
+      scriptDir: resolve('src/Core/gameScripts/pixi/performs'),
+      managerDir: resolve('src/Core/util/pixiPerformManager'),
+      outputFile: 'initRegister.ts',
+      watchDebounce: 100,
+      clearWhenClose: false,
+    }),
     webgalGo(),
     // @ts-ignore
     // visualizer(),
